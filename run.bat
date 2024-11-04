c366fd09210231f5e7912adb677a97528333a410
Tamás Polgár <polgarey@gmail.com>
2024-11-04 11:01:40 +0100

    Improvements

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @REM x64 backend for faster compile time
 rm .\asterisk_debug.exe
-jai .\first.jai -x64 && .\asterisk_debug.exe
+jai .\first.jai -x64 && .\asterisk_debug.exe .\src\draw.jai .\src\editor.jai
