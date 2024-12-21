d3ab73ec60e7819a350e5b9d3ab0b0480f2a0798
Polgár Tamás <polgarey@gmail.com>
2024-12-21 18:02:17 +0100

    Hacky solution for the expand selection by a word editor action

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 
 @REM call .\compile.bat && .\build\asterisk_devbuild.exe .\..\sample.txt
-call .\compile.bat && .\build\asterisk_devbuild.exe
+call .\compile.bat && .\build\asterisk_devbuild.exe .\..\.build\.added_strings_w3.jai
