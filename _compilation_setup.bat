a65bed6ead35d69f7df005fe9b3ffca3e144d70d
Polgár Tamás <polgarey@gmail.com>
2025-06-06 18:19:51 +0200

    WIP

--- _compilation_setup.bat
+++
@@ -1,8 +0,0 @@
-set ARGS=
-
-IF "%~1"=="profile" (
-    set ARGS=-plug Iprof %ARGS%
-)
-IF "%~1"=="profile-full" (
-    set ARGS=-plug Iprof -modules %ARGS%
-)
--- compile.bat
+++ compile.bat
@@ -1,7 +1,3 @@
 @echo off
 
-call _compilation_setup.bat %*
-
-@echo on
-
-jai .\build.jai %ARGS% - bake_font
+jai .\first.jai
